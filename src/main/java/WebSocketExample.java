3bea8e13127b13b4a6a644643b67aaeba97c7eb4
vitaly <bikevit2008@rambler.ru>
2015-08-08 22:30:26 +0300

    WebSocketExample with Hello! Chat, re-send message

--- src/main/java/WebSocketExample.java
+++ src/main/java/WebSocketExample.java
@@ -2,6 +2,8 @@ import de.neuland.jade4j.Jade4J;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.http.WebSocketBase;
+import io.vertx.core.http.WebSocketFrame;
 import io.vertx.ext.web.Router;
 
 import java.io.IOException;
@@ -39,9 +41,11 @@ public class WebSocketExample {
 
         });
         server
-                .websocketHandler(serverWebSocket -> {
-                    serverWebSocket.writeFrame(io.vertx.core.http.WebSocketFrame.textFrame("Hi!", true));
-                })
+                .websocketHandler(WebSocketBase -> {
+            WebSocketBase.frameHandler(WebSocketFrame -> {
+                WebSocketBase.writeFinalTextFrame(WebSocketFrame.textData());
+            });
+        })
                 .requestHandler(router::accept)
                 .listen(8080);
     }
