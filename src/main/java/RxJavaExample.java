8cfd97ebc3fab3749c71930d1d0f8d1ffddac7c3
vitaly <bikevit2008@rambler.ru>
2015-09-03 19:08:12 +0300

    Event-bus Ping-Pong with WebSocket

---
+++ src/main/java/RxJavaExample.java
@@ -0,0 +1,59 @@
+/**
+ * Created by vitaly on 11.08.15.
+ */
+import de.neuland.jade4j.Jade4J;
+import io.vertx.rxjava.core.*;
+import io.vertx.rxjava.core.eventbus.EventBus;
+import io.vertx.rxjava.core.http.HttpServer;
+import io.vertx.rxjava.core.http.HttpServerResponse;
+
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RxJavaExample {
+
+    private static final String ADDRESS = "websocket-test";
+    public static void main(String[] args) {
+
+        Vertx vertx = Vertx.vertx();
+
+        HttpServer server = vertx.createHttpServer();
+
+        server.requestStream().toObservable().subscribe(req -> {
+            HttpServerResponse resp = req.response();
+            resp.putHeader("content-type", "text/html");
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            // Write to the response and end it
+            try {
+                String html = Jade4J.render("web/templates/websocket.jade", model);
+
+                resp.end(html);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        EventBus eb = vertx.eventBus();
+
+        server.websocketStream().toObservable().subscribe(
+                socket -> {
+                    socket.toObservable().subscribe(buffer -> {
+                        System.out.println("Got message " + buffer.toString("UTF-8"));
+                        eb.consumer(ADDRESS).toObservable().subscribe(message -> {
+                            socket.writeFinalTextFrame(message.body().toString());
+                            System.out.println("Received " + message.body());
+                        });
+                        eb.publish(ADDRESS, buffer.toString("UTF-8"));
+                    });
+
+                },
+                failure -> System.out.println("Should never be called"),
+                () -> {
+                    System.out.println("Subscription ended or server closed");
+                }
+        );
+        server.listenObservable(8080);
+    }
+}
