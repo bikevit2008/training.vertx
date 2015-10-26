af0792c33be3f6ad06e2778e81792ec328b1faf0
bikevit2008 <bikevit2008@rambler.ru>
2015-10-26 22:47:58 +0300

    Test commit with bugs

--- src/main/java/controller/forms/post/CreateRoomHandler.java
+++ src/main/java/controller/forms/post/CreateRoomHandler.java
@@ -1,10 +1,19 @@
 package controller.forms.post;
 
+import de.neuland.jade4j.Jade4J;
 import io.vertx.core.Handler;
+import io.vertx.rxjava.core.http.HttpServerResponse;
 import io.vertx.rxjava.ext.web.RoutingContext;
 import model.entity.Room;
 import service.RoomService;
 import service.factory.ServiceFactory;
+import utils.NamesRoomGenerator;
+import utils.RoomNameGenerator;
+import utils.URLParser;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Vitaly on 21.10.15.
@@ -16,14 +25,36 @@ public class CreateRoomHandler implements Handler<RoutingContext> {
 
     @Override
     public void handle(RoutingContext routingContext) {
-        String linkValue = routingContext.request().getFormAttribute("linkValue");
 
-        Room room = new Room(linkValue);
+        String linkValueVideo = routingContext.request().getFormAttribute("linkValue");
+        String provider = URLParser.getProvider(linkValueVideo);
+        String videoId = URLParser.getVideoID(linkValueVideo);
+        System.out.println("Provider: " + provider + " VideoID: " + videoId);
+        String errorMessage = "Link is not valid";
+
+        if(provider != errorMessage && videoId != errorMessage){
+            String roomUrl = NamesRoomGenerator.GenerateUrl();
+        Room room = new Room(roomUrl, provider, videoId);
 
         roomService.addRoom(room);
 
         routingContext.response().setStatusCode(301);
-        routingContext.response().putHeader("Location", "/room/gg");
+        routingContext.response().putHeader("Location", "/room/" + roomUrl);
         routingContext.response().end();
+        }
+        else{
+            HttpServerResponse resp = routingContext.response();
+            resp.putHeader("content-type", "text/html");
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            // Write to the response and end it
+            try {
+                String html = Jade4J.render("web/templates/landing.jade", model);
+
+                resp.end(html);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
