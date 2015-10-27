3af1ece53caef7ac0e878d662b99ad798b0acb39
bikevit2008 <bikevit2008@rambler.ru>
2015-10-27 20:30:06 +0300

    New classes

---
+++ src/main/java/controller/ChangesMethods.java
@@ -0,0 +1,17 @@
+package controller;
+
+import service.RoomService;
+import service.UserService;
+import service.factory.ServiceFactory;
+
+/**
+ * Created by Vitaly on 27.10.15.
+ */
+public class ChangesMethods {
+
+    private static UserService userService = ServiceFactory.getUserService();
+    private static RoomService roomService = ServiceFactory.getRoomService();
+
+
+
+}
